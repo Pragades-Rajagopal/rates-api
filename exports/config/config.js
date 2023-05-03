287b3ec5a560ac8e209bf8dfc9aeb377630f1214
Pragades Rajagopal <66718413+Pragades-Rajagopal@users.noreply.github.com>
2023-05-03 17:06:38 +0000

    adds export feature

---
+++ exports/config/config.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+module.exports = {
+    exportPath: path.resolve(__dirname, '../')
+}
--- plugins/Rate.js
+++ plugins/Rate.js
@@ -1,6 +1,9 @@
 /// <reference path="../global.d.ts" />
 'use strict'
 const moment = require('moment');
+const csv = require('fast-csv');
+const fs = require('fs');
+const { exportPath } = require('../exports/config/config')
 
 /** @param {import('fastify').FastifyInstance} app */
 module.exports = async function (app) {
@@ -11,4 +14,39 @@ module.exports = async function (app) {
             return await originalSave(options);
         }
     })
+
+    app.get(
+        '/export',
+        exportReport
+    )
+}
+
+const exportReport = async (request, response) => {
+    try {
+        const data = await app.platformatic.entities.rate.find({
+            limit: 100
+        });
+        const filePath = exportPath;
+        let time = moment().utcOffset("+05:30").format('YYYYMMDDhhmmss');
+        const filename = '/export_' + time + '.csv';
+        const endPath = filePath + filename;
+        console.log("endPath  --- ", endPath);
+        const ws = fs.createWriteStream(endPath);
+        csv.write(
+            data,
+            { headers: true }
+        )
+            .on('finish', () => {
+                return {
+                    message: "file ready for download",
+                    download_link: ""
+                }
+            })
+            .pipe(ws)
+    } catch (error) {
+        return {
+            message: "error while exporting report",
+            download_link: ""
+        }
+    }
 }
